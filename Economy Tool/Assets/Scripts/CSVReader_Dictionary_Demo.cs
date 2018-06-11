a192451192c8595f7b5bf0c4f605c1d2cc67d8d6
Stelios Avramidis <avramids@gmail.com>
2018-06-11 16:46:59 +0100

    Making the udpate button

--- Economy Tool/Assets/Scripts/CSVReader_Dictionary_Demo.cs
+++ Economy Tool/Assets/Scripts/CSVReader_Dictionary_Demo.cs
@@ -6,7 +6,15 @@ using System.Collections.Generic;
 public class CSVReader_Dictionary_Demo : MonoBehaviour
 {
     public GameObject m_DropdownList;
-    Dropdown m_Dropdown;
+    public GameObject m_Level;
+    public GameObject m_Minutes;
+    public GameObject m_Number;
+
+    InputField  m_InputLevel;
+    InputField  m_InputMinutes;
+    InputField  m_InputNumber;
+    string      m_ActiveOption;
+    Dropdown    m_Dropdown;
     Dictionary<string, Dictionary<string, object>> data, data_2;
 
     void Awake()
@@ -14,13 +22,16 @@ public class CSVReader_Dictionary_Demo : MonoBehaviour
 
        data = CSVReader_Dictionary.Read("page_1");
        data_2 = CSVReader_Dictionary.Read("page_2");
-
     }
 
     // Use this for initialization
     void Start()
     {
         m_Dropdown      = m_DropdownList.GetComponent<Dropdown>();
+        m_InputLevel    = m_Level.GetComponent<InputField>();
+        m_InputMinutes  = m_Minutes.GetComponent<InputField>();
+        m_InputNumber   = m_Number.GetComponent<InputField>();
+
         m_Dropdown.onValueChanged.AddListener(delegate {
             DropdownValueChangedHandler(m_Dropdown);
         });
@@ -37,9 +48,37 @@ public class CSVReader_Dictionary_Demo : MonoBehaviour
 
   
     }
-
+    //Function notifying that the selection changed
     private void DropdownValueChangedHandler(Dropdown target)
     {
         Debug.Log("selected: " + target.captionText.text);
+        m_ActiveOption = target.captionText.text;
+        UpdateSelectedOption();
+    }
+
+    private void UpdateSelectedOption( )
+    {
+        object result;
+        if (data[m_ActiveOption].TryGetValue("Level", out result))
+        {
+            m_InputLevel.text = result.ToString();
+        }
+        if (data[m_ActiveOption].TryGetValue("Minutes", out result))
+        {
+            m_InputMinutes.text = result.ToString();
+        }
+        if (data[m_ActiveOption].TryGetValue("Number", out result))
+        {
+            m_InputNumber.text = result.ToString();
+        }
+    }
+
+    public void UpdateValues()
+    {
+
+        data[m_ActiveOption]["Level"]   = m_InputLevel.text;
+        data[m_ActiveOption]["Minutes"] = m_InputMinutes.text;
+        data[m_ActiveOption]["Number"]  = m_InputNumber.text;
+        Debug.Log("I updated the values");
     }
 }
