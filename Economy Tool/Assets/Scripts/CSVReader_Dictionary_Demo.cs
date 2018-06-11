eca252b9609874eee7061af3d51d246532b8dafb
Stelios Avramidis <avramids@gmail.com>
2018-06-11 14:57:48 +0100

    Making the script detect the option from the dropdown menu

--- Economy Tool/Assets/Scripts/CSVReader_Dictionary_Demo.cs
+++ Economy Tool/Assets/Scripts/CSVReader_Dictionary_Demo.cs
@@ -21,6 +21,9 @@ public class CSVReader_Dictionary_Demo : MonoBehaviour
     void Start()
     {
         m_Dropdown      = m_DropdownList.GetComponent<Dropdown>();
+        m_Dropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChangedHandler(m_Dropdown);
+        });
         foreach (KeyValuePair<string, Dictionary<string,object>> entry in data)
         {
             m_Dropdown.options.Add(new Dropdown.OptionData(entry.Key));
@@ -32,5 +35,11 @@ public class CSVReader_Dictionary_Demo : MonoBehaviour
     void Update()
     {
 
+  
+    }
+
+    private void DropdownValueChangedHandler(Dropdown target)
+    {
+        Debug.Log("selected: " + target.captionText.text);
     }
 }
