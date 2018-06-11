55c15b968665d1493c1e616565d06b7ad94cfc16
Stelios Avramidis <avramids@gmail.com>
2018-06-11 11:05:19 +0100

    Adding a new way to read the CSV file

---
+++ Economy Tool/Assets/Scripts/CSVReader_Dictionary_Demo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CSVReader_Dictionary_Demo : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+        List<Dictionary<string, object>> data = CSVReader_Dictionary.Read("page_2");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            //print("name " + data[i]["name"] + " " +
+            //       "age " + data[i]["age"] + " " +
+            //       "speed " + data[i]["speed"] + " " +
+            //       "desc " + data[i]["description"]);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
