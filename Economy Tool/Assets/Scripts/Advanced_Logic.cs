780114898eba87e24d420d4bee0a41f5bc38b93a
stylianos <avramids@gmail.com>
2018-06-16 18:12:28 +0100

    finished the advanced screen
    it now does everything that it should

--- Economy Tool/Assets/Scripts/Advanced_Logic.cs
+++ Economy Tool/Assets/Scripts/Advanced_Logic.cs
@@ -7,14 +7,22 @@ using System;
 public class Advanced_Logic : MonoBehaviour
 {
     public GameObject       m_DropdownList;
+    public GameObject       m_DropdownListMinutes;
+    public GameObject       m_DropdownListSessions;
     public GameObject       m_DropdownLabel;
     public GameObject       m_InputPair;
     public RectTransform    m_Content;
     public Text m_Output;
+    public Text m_LevelRequired;
+    public Text m_TimeToBuild;
+    public Text m_TimeToBuildDays;
+    public Text m_NumberofSessions;
 
     private bool m_OptionSet;
     string      m_ActiveOption;
     Dropdown    m_Dropdown;
+    Dropdown    m_Minutes;
+    Dropdown    m_SessionsPerDay;
     SortedDictionary<string, Dictionary<string, int>> data, data_2;
     Dictionary<string, GameObject> m_NameDictionary, m_InputDictionary;
 
@@ -31,10 +39,17 @@ public class Advanced_Logic : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        m_Dropdown      = m_DropdownList.GetComponent<Dropdown>();
-
+        m_Dropdown          = m_DropdownList.GetComponent<Dropdown>();
+        m_Minutes           = m_DropdownListMinutes.GetComponent<Dropdown>();
+        m_SessionsPerDay    = m_DropdownListSessions.GetComponent<Dropdown>();
         m_Dropdown.onValueChanged.AddListener(delegate {
             DropdownValueChangedHandler(m_Dropdown);
+        });
+        m_SessionsPerDay.onValueChanged.AddListener(delegate {
+            CalculateTimeToBuild();
+        });
+        m_Minutes.onValueChanged.AddListener(delegate {
+            CalculateTimeToBuild();
         });
         foreach (KeyValuePair<string, Dictionary<string,int>> entry in data_2)
         {
@@ -89,6 +104,7 @@ public class Advanced_Logic : MonoBehaviour
             //Get one by one the appropriate input fields and update their values
             m_InputDictionary[entry.Key].GetComponent<InputField>().text = entry.Value.ToString();
         }
+        CalculateTimeToBuild();
     }
 
     public void UpdateValues()
@@ -102,6 +118,7 @@ public class Advanced_Logic : MonoBehaviour
         Debug.Log("I updated the values");
         m_Output.color = Color.white;
         m_Output.text = "I updated the values";
+        CalculateTimeToBuild();
     }
 
     public void CheckForLocks()
@@ -112,9 +129,39 @@ public class Advanced_Logic : MonoBehaviour
         {
             if ( data_2[entry.Key][entry.Key] != 0 )
             {
-                m_Output.text = m_Output.text + " DeadLock detected !! " + entry.Key + " requires itself " + "\n";
+                m_Output.text = m_Output.text + " DeadLock detected! " + entry.Key + " requires itself " + "\n";
             
             }
         }
+    }
+
+    public void CalculateTimeToBuild()
+    {
+        float time_to_build = 0 ;
+        float level_required = 0;
+        float days = 0;
+        //grab everything with that key to find how many minutes it takes to build something. 
+        foreach (KeyValuePair<string, int> entry in data_2[m_ActiveOption])
+        {
+            //Basically get the minutes required per resource and multiply by how many resources are needed 
+            if ( entry.Value > 0)
+            {
+                time_to_build += data[entry.Key]["Minutes"] * entry.Value;
+                if (data[entry.Key]["Level"] > level_required)
+                {
+                    level_required = data[entry.Key]["Level"];
+                }
+            }
+           
+        }
+        //Debug.Log("This is teh currrnet selection frot the minutes per sessions" + m_Minutes.captionText.text);
+        //Debug.Log("This is the otehr one" + m_SessionsPerDay.captionText.text);
+        m_TimeToBuild.text = time_to_build.ToString();
+        m_LevelRequired.text = level_required.ToString();
+        int minutes_per_sessions = Int32.Parse(m_Minutes.captionText.text);
+        int sessions_per_day = Int32.Parse(m_SessionsPerDay.captionText.text);
+
+        days = time_to_build / (minutes_per_sessions * sessions_per_day);
+        m_TimeToBuildDays.text = days.ToString();
     }
 }
